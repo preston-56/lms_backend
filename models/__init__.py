31977e0d10c756ddc9d2d0f24d7bcf964dfe4d1f
Preston Osoro <prestonosoro56@gmail.com>
2025-05-08 05:03:16 +0300

    feat(models): centralize model exports for cleaner imports across modules

---
+++ models/__init__.py
@@ -0,0 +1,5 @@
+from course.models.course import Course
+from email_service.models.service import EmailLog
+from faculty.models.faculty import Instructor
+from notification.models.notification import Notification
+from user.models.user import User
